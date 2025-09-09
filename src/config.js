4f0e14fb7a3515ed7e50fd80d254b3acfa56530a
Nahyan Shanid <nahyanshanid2321q@gmail.com>
2025-09-09 10:01:26 +0530

    fixed frontend errors

--- src/config.js
+++ src/config.js
@@ -12,7 +12,7 @@ export const API = {
     MY_ANSWERS: `${BASE_URL}/answers/mine`,
     MY_SUBMISSIONS: `${BASE_URL}/answers/submissions/mine`,
   },
-  FEEDBACK: `http://localhost:5000/feedback/viewfeedback`,
-  SUBMISSIONS: `http://localhost:5000/submit/viewsubmit`,
+  FEEDBACK: `${BASE_URL}/feedback/viewfeedback`,
+  SUBMISSIONS: `${BASE_URL}/submit/viewsubmit`,
 };
 export default { BASE_URL };
